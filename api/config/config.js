ef851789180c0307672edb07a0cf1b79a7ca4b34
webeao <webeao@gmail.com>
2021-04-01 12:44:21 -0500

    first commit

---
+++ api/config/config.js
@@ -0,0 +1,23 @@
+//configurar las variables de entorno para el puerto  
+process.env.PORT = process.env.PORT || 3002;
+
+//Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//============================
+//Base de datos
+//============================
+let  urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+    urlDB ='mongodb://superAdmin:Alan123*@179.50.90.135:20723/alert-planet?authSource=admin'
+//  urlDB = 'mongodb://localhost:27017/alert-planet'
+}else{
+    urlDB = 'mongodb://179.50.90.135:20723/alert-planet'
+}
+process.env.URLDB =urlDB;
+
+process.env.SEED = process.env.SEED || 'seguridad alert-planet';
+
+process.env.CADUCIDAD_TOKEN = '48h';
